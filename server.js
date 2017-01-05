5f9556ef7ef8cf7da165c66b94ff183051e20c2c
Zaid <zaliaslab@gmail.com>
2017-01-05 21:45:14 +0000

    Added Full API Behaviour

--- server.js
+++ server.js
@@ -1,10 +1,33 @@
 var express = require('express');
+var moment = require('moment');
 var app = express();
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-app.listen(8080, function () {
-  console.log('Example app listening on port 3000!');
+app.get('/:inputDate', function(req, res) {
+  var inputDate = req.params.inputDate;
+  var eTime = parseInt(inputDate);
+  
+  var naturalFormat = "MMMM DD, YYYY";  
+
+  var result = {
+    "unix" : null,
+    "natural" : null
+  };
+
+  if (moment(inputDate, naturalFormat, true).isValid()) {
+    result.unix = Date.parse(inputDate) / 1000;
+    result.natural = inputDate;
+  }
+  else if (eTime) {
+    result.unix = eTime;
+    result.natural = moment(eTime).format(naturalFormat);
+  }
+  res.send(result);
+});
+
+app.listen(process.env.PORT || 8080, process.env.IP, function () {
+  console.log('App listening at ', process.env.PORT  + ":" + process.env.IP);
 });
